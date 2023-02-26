2bf2e2d7d1d49315b9b861499e546505270d7f3f
Rahul Walse <rahul_walse@hotmail.com>
2023-02-26 19:33:46 +0530

    Provide menu options.

---
+++ SmallestDivisor/Main.cpp
@@ -0,0 +1,36 @@
+// SmallestDivisor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Divisor.h"
+
+int main()
+{
+    bool stay = true;
+    int option = 0;
+    int number;
+
+    while (stay)
+    {
+        std::cout << "\n1. Find smallest divisor for given number\n2. Exit";
+        std::cout << "\nChoose an option: ";
+        std::cin >> option;
+
+        switch (option)
+        {
+            case 1: std::cout << "\nEnter a number: ";
+                    std::cin >> number;
+                    std::cout << "\nSmallest divisor of " << number << " is " << smallest_divisor(number) << std::endl;
+                    break;
+
+            case 2: std::cout << "\nExiting the program...";
+                    stay = false;
+                    break;
+
+            default:std::cout << "\nInvalid option! Please choose from the options provided.\n";
+                    break;
+        }
+    }
+
+    return 0;
+}
