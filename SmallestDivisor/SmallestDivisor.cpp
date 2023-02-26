60db3ed9cf1d85468fcd8731e8242b803cf8dceb
Rahul Walse <rahul_walse@hotmail.com>
2023-02-26 19:23:14 +0530

    Renamed file

--- SmallestDivisor/SmallestDivisor.cpp
+++ SmallestDivisor/SmallestDivisor.cpp
@@ -1,16 +1,32 @@
-// SmallestDivisor.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+    Source file implementing the method to calculate the smallest divisor for given
+    integer, by generating divisors which are less than square root of given integer.
+*/
 
-#include <iostream>
 #include "Divisor.h"
+#include "MyMath.h"
 
-int main()
+int smallest_divisor(const double number)
 {
-    int number;
-    std::cout << "\nEnter a number to find smallest divisor: ";
-    std::cin >> number;
+    int sqr_root = 0;
+    int divisor = 0;
+    //- If the number is even then smallest divisor is 2
+    if (static_cast<int>(number) % 2 == 0)
+        return 2;
+    else
+    {
+        //- Calculate the square root of number
+        sqr_root = static_cast<int>(square_root(number));
+        //- Initialize divisor to 3
+        divisor = 3;
 
-    std::cout << "\nSmallest divisor of " << number << " is " << smallest_divisor(number);
+        //- Generate next odd divisor
+        while (static_cast<int>(number) % divisor != 0 && divisor < sqr_root)
+            divisor += 2;
 
-    return 0;
+        if (static_cast<int>(number) % divisor == 0)
+            return divisor;
+        else
+            return 1;
+    }
 }
